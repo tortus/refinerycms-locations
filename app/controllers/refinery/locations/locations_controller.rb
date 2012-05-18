4eff285cfaa0be1ccd38566d5369cc927def2d32
William Makley <wmakley@tortus.com>
2012-05-18 16:45:13 -0400

    present location in locations#show

--- app/controllers/refinery/locations/locations_controller.rb
+++ app/controllers/refinery/locations/locations_controller.rb
@@ -16,7 +16,7 @@ module Refinery
 
         # you can use meta fields from your model instead (e.g. browser_title)
         # by swapping @page for @location in the line below:
-        present(@page)
+        present(@location)
       end
 
     protected

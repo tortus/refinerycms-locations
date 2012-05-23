b4a94082ae1a36f55029ab09c08d214a92818351
William Makley <wmakley@tortus.com>
2012-05-23 11:55:27 -0400

    location now updates its page when its title/url changes

--- app/models/refinery/locations/location.rb
+++ app/models/refinery/locations/location.rb
@@ -15,51 +15,65 @@ module Refinery
       validates :name, :presence => true, :uniqueness => true
 
       belongs_to :image, :class_name => '::Refinery::Image'
-      
+
       # meta
       def title; name; end
-      
+
       def city_state_zip
         "#{city}, #{state} #{zip}"
       end
-      
+
       # returns phone number with all punctuation removed and "+1" prepended
       def international_phone
         phone.gsub(/[^\d]/, "").prepend('+1')
       end
-      
+
       # returns phone number with all extra punctuation from the CMS
       def formatted_phone
         phone
       end
-      
+
       def self.active; where(:hidden => false); end
       def self.live; active; end
       def self.by_position; order("position ASC"); end
       def self.by_name; order("name ASC"); end
-      
+
       def self.url
         "/locations"
       end
       def self.page
         ::Refinery::Page.where(:link_url => url).first
       end
-      
+
       def url
         "#{self.class.url}/#{friendly_id}"
       end
+
+      # Returns the previous url prior to any attribute assignments
+      def url_was
+        "#{self.class.url}/#{slug_was}"
+      end
+
       def page
         @page ||= ::Refinery::Page.where(:link_url => url).first
       end
-      
+
+      def reload
+        @page = nil
+        super
+      end
+
       after_create :create_page
       after_destroy :destroy_page
-      
+
+      before_update :remember_page
+      after_update :update_page
+
     private
 
       def create_page
-        if parent = self.class.page
-          self.class.transaction do
+        self.class.transaction do
+          if parent = self.class.page
             page = parent.children.create!(
               :title => name,
               :link_url => url,
@@ -69,9 +83,24 @@ module Refinery
           end
         end
       end
-      
+
       def destroy_page
         page.destroy! if page
+      end
+
+      def remember_page
+        @previous_page = ::Refinery::Page.where(:link_url => url_was).first
+        @page = nil
+      end
+
+      def update_page
+        if @previous_page
+          @previous_page.update_attributes(
+            :title => name,
+            :link_url => url,
+            :menu_match => "^#{url}(\/|\/.+?|)$"
+          )
+        end
       end
 
     end
